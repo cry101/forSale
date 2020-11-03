a7714e1a1cfde2e1db8378c24248cffb282442f3
Cary <919907366@qq.com>
2020-11-03 17:37:27 +0800

    1

--- deploy.bat
+++ deploy.bat
@@ -3,10 +3,10 @@
 echo "Start deployment"
 cd C:\App\forSale
 echo "pulling source code..."
-@REM git reset --hard origin/master
-@REM git clean -f
-@REM git pull
-@REM git checkout master
-@REM npm install
+git reset --hard origin/master
+git clean -f
+git pull
+git checkout master
+npm install
 npm run start
 echo "Finished."
