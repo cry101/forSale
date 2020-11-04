be15b56c8a718dbd7d07d7134c3981ac420db7a6
Cary <919907366@qq.com>
2020-11-04 09:05:48 +0800

    自动化部署

--- deploy.bat
+++ deploy.bat
@@ -1,12 +1,12 @@
 @echo off
 
 echo "Start deployment"
-cd C:\App\forSale
+@REM cd C:\App\forSale
 echo "pulling source code..."
 git reset --hard origin/master
 git clean -f
 git pull
 git checkout master
-npm install
+call npm install
 npm run start
 echo "Finished."
